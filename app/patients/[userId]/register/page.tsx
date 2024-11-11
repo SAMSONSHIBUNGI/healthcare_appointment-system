ca88f61233e10e14a52aa74901d7521a9d44bd16
SAMSONSHIBUNGI <samsonshibung@gmail.com>
2024-11-11 23:18:53 +0300

    Errors

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,21 +1,19 @@
 import Image from "next/image";
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
-import { PatientForm } from "@/components/forms/PatientForm";
-import { PasskeyModal } from "@/components/PasskeyModal";
+import RegisterForm from "@/components/forms/RegisterForm";
+import { getPatient, getUser } from "@/lib/actions/patient.actions";
 
-type Props = { searchParams: Promise<{ [key: string]: string | string[] | undefined }>}
+const Register = async ({ params: { userId } }: SearchParamProps) => {
+  const user = await getUser(userId);
+  const patient = await getPatient(userId);
 
-const Home = async ({ searchParams }: Props) => {
-  const searchParam = await searchParams;
-  
+  if (patient) redirect(`/patients/${userId}/new-appointment`);
 
   return (
     <div className="flex h-screen max-h-screen">
-      {searchParam?.isAdmin && <PasskeyModal />}
-
-      <section className="remove-scrollbar container my-auto">
-        <div className="sub-container max-w-[496px]">
+      <section className="remove-scrollbar container">
+        <div className="sub-container max-w-[860px] flex-1 flex-col py-10">
           <Image
             src="/assets/icons/logo-full.svg"
             height={1000}
@@ -24,28 +22,21 @@ const Home = async ({ searchParams }: Props) => {
             className="mb-12 h-10 w-fit"
           />
 
-          <PatientForm />
+          <RegisterForm user={user} />
 
-          <div className="text-14-regular mt-20 flex justify-between">
-            <p className="justify-items-end text-dark-600 xl:text-left">
-              © 2024 CarePluse
-            </p>
-            <Link href="/?admin=true" className="text-green-500">
-              Admin
-            </Link>
-          </div>
+          <p className="copyright py-12">© 2024 CarePluse</p>
         </div>
       </section>
 
       <Image
-        src="/assets/images/onboarding-img.png"
+        src="/assets/images/register-img.png"
         height={1000}
         width={1000}
         alt="patient"
-        className="side-img max-w-[50%]"
+        className="side-img max-w-[390px]"
       />
     </div>
   );
 };
 
-export default Home
+export default Register;
